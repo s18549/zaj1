600303a78b863dbf88e2df272adffc2c93224e9c
s18549 <s18549@pjwstk.edupl>
2024-03-17 20:13:14 +0100

    commit feature-avarage branch

--- ZAD1/ZAD1/Program.cs
+++ ZAD1/ZAD1/Program.cs
@@ -1,5 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("MOD 1");
-Console.WriteLine("MOD 2");
-Console.WriteLine("MOD 3");
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { 5, 10, 15, 20, 25 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine("Średnia: " + average);
+    }
+
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Tablica liczb nie może być pusta.");
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+}
